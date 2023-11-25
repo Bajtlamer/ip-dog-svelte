a3ce8239d91055b7bb16417d460729d8e17b6169
Radek Roža <roza@Rozas-MacBook-Pro.local>
2023-11-25 12:06:05 +0100

    Fixed confirmation dialog buttons align

--- src/modals/confirmation-dialog.svelte
+++ src/modals/confirmation-dialog.svelte
@@ -7,7 +7,7 @@
 	export let modal: any;
 </script>
 
-<div class="relative w-96 items-center mx-auto bg-gray-800 p-2 rounded-lg">
+<div class="relative min-w-96 items-center mx-auto bg-gray-800 p-2 rounded-lg">
 	<!-- Modal content -->
 	<div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
 		<!-- Modal header -->
@@ -23,18 +23,18 @@
       </p>
 		</div>
 		<!-- Modal footer -->
-		<div class="flex items-center p-4 border-gray-200 rounded-b dark:border-gray-600">
+		<div class="block text-right place-items-end items-end p-4 border-gray-200 rounded-b dark:border-gray-600">
+      <button
+      on:click|preventDefault={() => dialog.close()}
+      type="button"
+      class="w-20 ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-2 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+      >Cancel</button
+			>
 			<button
         on:click|preventDefault={success}
 				type="submit"
-				class="w-20 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+				class="w-20 ml-2 text-white bg-red-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-2 py-2.5 text-center dark:bg-red-700 dark:hover:bg-red-600 dark:focus:ring-red-600"
 				>Delete</button
-			>
-			<button
-				on:click|preventDefault={() => dialog.close()}
-				type="button"
-				class="w-20 ms-3 text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-2 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
-				>Cancel</button
 			>
 		</div>
 	</div>
