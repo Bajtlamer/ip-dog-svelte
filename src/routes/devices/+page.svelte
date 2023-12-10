ed592721297b856bf2561a57dc4c5293dcd61f48
Radek Roza <roza@rrsoft.cz>
2023-12-10 22:31:35 +0100

    Refactor device search functionality to search keyword in all device properties

--- src/routes/devices/+page.svelte
+++ src/routes/devices/+page.svelte
@@ -7,18 +7,23 @@
 	export let data: PageData;
 	let searching: string = '';
 	let items: IDevice[] = [];
+	let excludedKeys: string[] = ['owner', 'id', 'subnetId'];
 
 	$: devices = data.devices;
 	$: devicesCount = data.devices.length;
 
 	const searchHandler = (event: CustomEvent<string>) => {
 		searching = event.detail;
-		// console.log(searching);
-		items = devices.filter((device) => {
-			return device.address.toLowerCase().includes(searching.toLowerCase());
-		});
-		devicesCount = items.length;
 
+		if (devices.length === 0) return;
+
+		items = devices.filter((device: IDevice) => {
+			return Object.keys(device).some((key) => {
+				return (excludedKeys.includes(key)) ? false : device[key as keyof IDevice]?.toString().toLocaleLowerCase().indexOf(searching) !== -1;
+			});
+		});
+
+		devicesCount = items.length;
 	};
 
 	onMount(() => {
@@ -28,46 +33,47 @@
 </script>
 
 <div class="items-center min-h-screen max-w-full mx-auto bg-gray-800 lg:p-20">
-	<div class="block max-w-screen-sm mx-auto p-2">
-		<h1 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-			Search for Subnet list
+	<div class="block max-w-screen-sm mx-auto px-2">
+		<h1 class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+			Search for Devices
 		</h1>
+		<span class="text-xs text-white">Found devices: {devicesCount}</span>
 	</div>
-	<div class="block max-w-screen-sm mx-auto py-3 px-1">
+	<div class="block max-w-screen-sm mx-auto pb-4 px-1">
 		<SearchDevices on:onSearch={searchHandler} {searching} />
 	</div>
 
-	<div class="max-w-screen-sm p-6 mx-auto bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-900 dark:border-gray-700">
-	{#if devicesCount > 0}
-		<ul>
-			{#each items as device, index}
-			<li
-			class:border-b={devicesCount - 1 > index}
-			class=" border-gray-600">
-				<dl class="max-w-md text-gray-900 dark:text-white">
-					<div class="flex flex-col py-3">
-						<dd class="text-lg font-semibold">
-							<a href={`/servers/${device.owner.serverId}/${device.subnetId}`}>
-								{#if device.description}
-									{device.description}
-								{:else}
-									N/A
-								{/if}
-							</a>
-						</dd>
-						<dt class="text-gray-500 md:text-md dark:text-gray-400">
-							<a href={`/servers/${device.owner.serverId}/${device.subnetId}`}>
-								{device.hostname}
-								<span class="text-sm">
-									({device.address})
-								</span>
-							</a>
-						</dt>
-					</div>
-				</dl>
-			</li>
-			{/each}
-		</ul>
+	<div
+		class="max-w-screen-sm p-6 mx-auto bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-900 dark:border-gray-700"
+	>
+		{#if devicesCount > 0}
+			<ul>
+				{#each items as device, index}
+					<li class:border-b={devicesCount - 1 > index} class=" border-gray-600">
+						<dl class="max-w-md text-gray-900 dark:text-white">
+							<div class="flex flex-col py-3">
+								<dd class="text-lg font-semibold">
+									<a href={`/servers/${device.owner.serverId}/${device.subnetId}`}>
+										{#if device.description}
+											{device.description}
+										{:else}
+											N/A
+										{/if}
+									</a>
+								</dd>
+								<dt class="text-gray-500 md:text-md dark:text-gray-400">
+									<a href={`/servers/${device.owner.serverId}/${device.subnetId}`}>
+										{device.hostname}
+										<span class="text-sm">
+											({device.address})
+										</span>
+									</a>
+								</dt>
+							</div>
+						</dl>
+					</li>
+				{/each}
+			</ul>
 		{:else}
 			<div class="text-gray-900 dark:text-white">No result found</div>
 		{/if}
